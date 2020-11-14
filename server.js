e5ae6cef83e45a44a245cb8e427598f59d3cfe95
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2020-11-14 15:34:59 +0100

    finished author routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -23,6 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(methodOverride("_method"));
 
 app.use(expressLayouts);
 app.use(express.static("public"));
