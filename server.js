8c6ca3150a82cf87d2c24a246df92284b67cf580
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2020-11-14 10:02:55 +0100

    fixes bugs and adds search functionality

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ db.once("open", () => console.log("Connected to Mongoose"));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 app.listen(process.env.PORT || 3000);
